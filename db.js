b087921b35aca44d2a4a00f1a0c18af00f764f03
Nico <luez.nicolas@gmail.com>
2021-12-04 14:22:13 +0100

    add env + localhost db

--- db.js
+++ db.js
@@ -6,7 +6,7 @@ class Database {
   }
 
    async connect() {
-    return  await mongoose.connect('mongodb+srv://pampa:euI9WXvYjZlMgVjj@spicy.jt95g.mongodb.net/spicy?retryWrites=true&w=majority',
+    return  await mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/spicy",
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
